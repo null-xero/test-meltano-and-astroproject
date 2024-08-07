5d9295847cd46924050914c3428df6d745cd746f
Ihsan Rais <ihsan.rais22@gmail.com>
2024-08-08 04:05:31 +0700

    version edan

--- coba/astro_project/dags/ecommerce_pipeline.py
+++ coba/astro_project/dags/ecommerce_pipeline.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.providers.dbt.cloud.hooks.dbt import DbtCloudHook
+# from airflow.providers.dbt.cloud.hooks.dbt import DbtCloudHook
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
 default_args = {
@@ -12,17 +13,19 @@ default_args = {
 with DAG('ecommerce_pipeline', default_args=default_args, schedule_interval='@daily') as dag:
 
     # Task untuk menjalankan Meltano
-    run_meltano = BashOperator(
+    run_meltano = DockerOperator(
         task_id='run_meltano',
-        bash_command='cd /project && meltano elt tap-csv target-postgres',
+        image='meltano',
+        container_name='meltano_{{ run_id }}',
+        command='elt tap-csv target-snowflake',
         dag=dag
     )
 
-    # Task untuk menjalankan dbt
-    run_dbt = BashOperator(
-        task_id='run_dbt',
-        bash_command='cd /project && dbt run',
-        dag=dag
-    )
+    # # Task untuk menjalankan dbt
+    # run_dbt = BashOperator(
+    #     task_id='run_dbt',
+    #     bash_command='cd /project && dbt run',
+    #     dag=dag
+    # )
 
-    run_meltano >> run_dbt
+    # run_meltano >> run_dbt
